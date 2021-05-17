0e31f40287df42a07add458ed5f2c105b068e219
ARAVINDHAN P <77089470+aravindhan-p-123@users.noreply.github.com>
2021-05-17 19:52:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,88 +1,90 @@
-<?php
 
-echo "\n";
-echo "<!doctype html>\n";
-echo "<html lang=\"en\">\n";
-echo "\n";
-echo "<head>\n";
-echo "    <link rel=\"shortcut icon\" href=\"D.JPG\" type=\"image/x-icon\">\n";
-echo "    <title>DONATE THE NEED</title>\n";
-echo "    <style>\n";
-echo "        .head {\n";
-echo "            position: relative;\n";
-echo "            background-color:white;\n";
-echo "            opacity: 0.7;\n";
-echo "            border-radius: 10px;\n";
+<!doctype html>
+<html lang="en">
 
-echo "        }\n";
-echo "        .footer {\n";
-echo "            position: relative;\n";
-echo "            border-radius: 10px;\n";
-echo "            margin-top: 10px;\n";
-echo "            margin-left: 50px;\n";
-echo "            margin-right: 50px;\n";
-echo "            margin-bottom: 10PX;\n";
-echo "            left: 20px;\n";
-echo "            right: 20px;\n";
-echo "            bottom: 20px;\n";
-echo "            height: 160px;\n";
-echo "        }\n";
-echo "        \n";
-echo "        .container{\n";
-echo "            position: relative;\n";
-echo "            background-image: linear-gradient(to right, #fcf7f7, rgb(245, 239, 244));\n";
-echo "            opacity: 0.7;\n";
-echo "            border-radius: 10px;\n";
-echo "            margin-top: 100px;\n";
-echo "            margin-left: 400px;\n";
-echo "            margin-right: 400px;\n";
-echo "            margin-bottom: 100PX;\n";
-echo "        }\n";
-echo "        .center{\n";
-echo "\n";
-echo "            text-align: center;\n";
-echo "            background-image: url('ee.jpg');\n";
-echo "             background-repeat: no-repeat; \n";
-echo "             background-size:100%; \n";
-echo "             \n";
-echo "        }\n";
-echo "\n";
-echo "    </style>\n";
-echo "</head>\n";
-echo "\n";
-echo "<body class=\"center\" >\n";
-echo "    <div class='jtextfill' style='width:1200px;height:px;'>\n";
-echo "        <header class=\" head\" >\n";
-echo "                <img src=\"q.png\"  alt=\"logo\" height=\"110\" style= \"border-radius: 4px; padding-top: 4px;\">\n";
-echo "        </header>\n";
-echo "\n";
-echo "        <div class=\"container\">\n";
-echo "            <h2 style=\"font-weight: bold; font-family:  sans-serif; color:rgb(90, 89, 89); padding-top: 20PX;\">Task-3:Payment Gateway Integration<br>\n";
-echo "            Graduate Rotational Internship Program <br> The Sparks Foundation</h2>\n";
-echo "            <h3 style=\"color: rgb(253, 119, 197); font-family: sans-serif; padding-bottom: 25PX;\">DONATE US</h3>\n";
-echo "                <!-- Payment Button -->\n";
-echo "                <form style=\"padding: 5px;\"> \n";
-echo "                    <script src=\"https://checkout.razorpay.com/v1/payment-button.js\" data-payment_button_id=\"pl_HB7nPETSnYABb2\" async> \n";
-echo "                </script>  \n";
-echo "                </form>\n";
-echo "                \n";
-echo "            \n";
-echo "        </div>\n";
-echo "\n";
-echo "        <!-- Footer -->\n";
-echo "        <div class=\"footer\" style=\"background-color: #c965ff;padding: 5px; opacity: 0.7; \">\n";
-echo "\n";
-echo "            <!-- Footer Links -->\n";
-echo "            <div >\n";
-echo "                <p style=\"color:rgb(66, 66, 66); font-family: sans-serif;\">JOIN OUR HANDS AND HELP OTHERS<br>THE WORLD NEEDS YOUR CONCERN<br> HELP US<BR><BR><BR></p>\n";
-echo "            \n";
-echo "                <p style=\"color: aqua; text-align:end; padding-right:10px\">© 2021 Copyright : ARAVINDHAN P | THE SPARK FOUNDATION</p>\n";
-echo "            </div>\n";
-echo "        </div>\n";
-echo "    </div>\n";
-echo "\n";
-echo "</body>\n";
-echo "\n";
-echo "</html>";
+<head>
+    <link rel="shortcut icon" href="D.JPG" type="image/x-icon">
+    <title>DONATE THE NEED</title>
+    <style>
+        .head {
+            background-color:white;
+            opacity: 0.7;
+            border-radius: 10px;
+            margin-top: 10px;
+            margin-left: 50px;
+            margin-right: 50px;
+            margin-bottom: 10PX;
+            left: 20px;
+            right: 20px;
+            bottom: 100%;
+            height: 160px;
+        }
+        .footer {
+            position: relative;
+            border-radius: 10px;
+            margin-top: 10px;
+            margin-left: 50px;
+            margin-right: 50px;
+            margin-bottom: 10PX;
+            left: 20px;
+            right: 20px;
+            bottom: 100%;
+            height: 160px;
+        }
+        
+        .container{
+            position: relative;
+            background-image: linear-gradient(to right, #fcf7f7, rgb(245, 239, 244));
+            opacity: 0.7;
+            border-radius: 10px;
+            margin-top: 100px;
+            margin-left: 400px;
+            margin-right: 400px;
+            margin-bottom: 100PX;
+        }
+        .center{
 
-?>
+            text-align: center;
+            background-image: url('ee.jpg');
+             background-repeat: no-repeat; 
+             background-size: 100%; 
+             
+        }
+
+    </style>
+</head>
+
+<body class="center" >
+    <div class='jtextfill' style='width:1670px;height:px;'>
+        <header class=" head" >
+                <img src="q.png"  alt="logo" height="110" style= "border-radius: 4px; padding-top: 4px;">
+        </header>
+
+        <div class="container">
+            <h2 style="font-weight: bold; font-family:  sans-serif; color:rgb(90, 89, 89); padding-top: 20PX;">Task-3:Payment Gateway Integration<br>
+            Graduate Rotational Internship Program <br> The Sparks Foundation</h2>
+            <h3 style="color: rgb(253, 119, 197); font-family: sans-serif; padding-bottom: 25PX;">DONATE US</h3>
+                <!-- Payment Button -->
+                <form style="padding: 5px;"> 
+                    <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_HB7nPETSnYABb2" async> 
+                </script>  
+                </form>
+                
+            
+        </div>
+
+        <!-- Footer -->
+        <div class="footer" style="background-color: #c965ff;padding: 5px; opacity: 0.7; ">
+
+            <!-- Footer Links -->
+            <div >
+                <p style="color:rgb(66, 66, 66); font-family: sans-serif;">JOIN OUR HANDS AND HELP OTHERS<br>THE WORLD NEEDS YOUR CONCERN<br> HELP US<BR><BR><BR></p>
+            
+                <p style="color: aqua; text-align:end; padding-right:10px">© 2021 Copyright : ARAVINDHAN P | THE SPARK FOUNDATION</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
